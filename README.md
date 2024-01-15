8c8d8aa55fb36729f18aa05b007cca8d70d64f47
JoseTulcan <90582244+JoseTulcan@users.noreply.github.com>
2024-01-15 18:12:01 +0000

    Actualización

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# recomendador_de_peliculas
-Este repositorio contiene los archivos necesarios y la aplicación en python de un recomendador de peliculas
+# RECOMENDADOR DE PELÍCULAS CON MACHINE LEARNING. 
+# Proyecto final - Asignatura: Automatización y Control
+
+## Creadores:
+
+### José Luis Mera Tulcán - Deyson Mauricio Laos S.
+
+Este proyecto consiste en la elaboración de un recomendador de películas usando el algoritmo Cosine similarity con una interfaz creada mediante Streamlit
+
+[![Imagen representativa](img/fondo.jpg)](img/fondo.jpg)
+
+## Objetivos
+- Automatizar la busqueda de películas de características similares a la ingresada
+- Crear una interfaz con Streamlit que permita la visualización de las películas recomendadas
+
+## Descripción 
+Se dispone una base de datos obtenida desde Kaggle [**movies_kaggle.csv**](./datos_kaggle/movies.csv) que contiene el catálogo de películas que se usan en el entrenamiento del algoritmo de recomendación además de otra información de cada película, como generos, actores de reparto, director, descripción, y otros más. Tambien se cuenta con una base de datos de IMDB [**movies_IMDb.csv**](./datos_IMDb/movie_metadata.csv) que contiene un catálogo similar de pellículas con la diferencia de que dispone de un enlace para cada película que al hacer clic en el lleva a la plataforma de IMDb, permitiendo así al usuario tener toda la información de la película recomendada que es de su interés incluyendo el trailer de cada película. 
+
+### CONSTRUCCIÓN DEL MODELO DE RECOMENDACIÓN
+
+1. IMPORTACIÓN DE LIBRERÍAS
+Se importa las librerías necesarias para los siguientes propósitos:
+    - Creación de interfaz en Streamlit
+    - Vectorización de datos
+    - Algoritmo Cosine similarity
+    - Manejo de datos y de imagenes
+
+2. CARGA DE DATOS 
+Se carga los datos de las dos bases de datos y se asingan las películas que estén en las dos bases a una nueva variable.
+Una vez cargados los datos, se seleccionan las columnas o características que se usarán en el entrenamiento del algoritmo.
+Con la selección de las características lista es posible verificar las filas vacías y continuar en el siguiente paso.
+
+3. VECTORIZACIÓN DE DATOS
+Los datos seleccionados deben ser vectorizados, es decir que el texto que conforma los titulos de las películas debe convertirse en vectores numéricos que representan las características.
+
+4. SIMILITUD COSENO
+Con los datos vectorizados se puede ya hacer la aplicación del algoritmo de cosine similarity. Cuando el usuario ingresa una película se evalua si hay coincidencias con la base de datos, si es así se procede a ordenar las películas incluyendo sus indices por similitud con la película ingresada. 
+
+5. RECOMENDACIÓN DE PELÍCULAS
+Dado que ya se tiene la lista de películas similares, es decir las que se va a recomendar por su cercania en sus características, se consulta con el nombre de estas películas en la variable donde se guardan las películas que están en las dos bases de datos y se extrae el enlace de cada película. Finalmente son mostrados en la interfaz.
+
+## Resultados
+Se hicieron diferentes pruebas con seleccionando o ingresando diferentes películas de la base de datos, y para los diferentes casos se pudo observar que el algoritmo entrega otros títulos de películas en orden descendente por su similitud en las características de las películas que fueron seleccionadas. Además la interfaz permite aumnetar o disminuir correctamente el número de películas recomendadas.
+
+Se muestra a continuación la interfaz de este proyecto de recomendadr de películas.
+
+[![Imagen representativa](img/interfaz1.JPG)](img/interfaz1.JPG)
+
+Ahora observemos un ejemplo de como se ve el modelo en acción.
+
+[![Imagen representativa](img/interfaz2.JPG)](img/interfaz2.JPG)
+
+## Conclusiones
+Es posible construir un modelo de recomendación de películas basado en nuestra película favorita o película de interés usando el algoritmo de similitud de coseno. 
+Es importante mencionar que este modelo de recomendación funciona bien para este propósito, aunque puede mejorar en diferentes aspectos como incluir imagenes de cada película en la interfaz, y reducir el uso de las bases de datos a solo una, entre otras que considere oportuna.
---
+++ main.py
@@ -0,0 +1,211 @@
+import streamlit as st
+import pandas as pd
+import difflib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import base64
+import plotly.express as px
+
+df = px.data.iris()
+
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("./img/fondo.jpg")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+    background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("https://raw.githubusercontent.com/JoseTulcan/proyecto-final-automatizacion-y-control/7477858c19d499677d0f28b20490f561ebb8dcfd/fondo.jpg");
+    background-size: 100%;
+    background-position: top left;
+    background-repeat: repeat;
+    background-attachment: local;
+}}
+
+[data-testid="stSidebar"] > div:first-child {{
+    background-image: url("data:image/png;base64,{img}");
+    background-position: center; 
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+}}
+
+[data-testid="stHeader"] {{
+    background: rgba(0,0,0,0);
+}}
+
+[data-testid="stToolbar"] {{
+    right: 2rem;
+}}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Cambiar el tamaño y color del texto
+# Configuraciones para el título
+title_style = """
+    color: white;          /* Cambiar el color del título (por ejemplo, a verde) */
+    font-size: 36px;       /* Cambiar el tamaño del título (por ejemplo, a 36px) */
+    font-weight: bold;     /* Cambiar el grosor del título a negrita */
+"""
+
+# Configuraciones para el subtítulo
+subtitle_style = """
+    color: white;           /* Cambiar el color del subtítulo (por ejemplo, a azul) */
+    font-size: 24px;       /* Cambiar el tamaño del subtítulo (por ejemplo, a 24px) */
+    font-weight: bold;     /* Cambiar el grosor del subtítulo a negrita */
+"""
+
+# Mostrar el título de página
+st.markdown(
+    f"""
+    <h1 style='{title_style}'>
+        RECOMENDADOR DE PELÍCULAS
+    </h1>
+    """,
+    unsafe_allow_html=True
+)
+
+# Carga de datos
+movies_data = pd.read_csv('./datos_kaggle/movies.csv')
+
+movies_data2 = pd.read_csv('./datos_IMDB/movie_metadata.csv')
+
+movies_data2['index'] = movies_data2.index
+
+# Normalizar las columnas antes de realizar la comparación
+movies_data['title_normalized'] = movies_data['title'].str.lower().str.strip()
+movies_data2['movie_title_normalized'] = movies_data2['movie_title'].str.lower().str.strip()
+
+# Realizar un merge en las columnas normalizadas
+merged_df = pd.merge(movies_data, movies_data2, how='inner', left_on='title_normalized', right_on='movie_title_normalized')
+
+# Seleccionar las columnas deseadas
+selected_columns = ['title', 'movie_imdb_link', 'keywords','tagline', 'cast', 'director']
+
+# Guardar las coincidencias en una variable
+result_df = merged_df[selected_columns]
+
+# Iterando a través de cada fila en movies_data
+for index, row in movies_data.iterrows():
+    title_movies_data = row['title']
+
+#Seleccionar las características(columnas) de la base de datos a consideración
+selected_features = ['genres', 'keywords', 'tagline', 'cast', 'director']
+
+#Asignación de las características no existentes con espacio vacío
+for feature in selected_features:
+    movies_data[feature] = movies_data[feature].fillna('')
+
+#Combinar las características elegídas para el entrenamiento
+combined_features = movies_data['genres']+' '+movies_data['keywords']+' '+movies_data['tagline']+' '+movies_data['cast']+' '+movies_data['director']
+
+#Vectorizar las características
+vectorizer = TfidfVectorizer()
+
+#Ajustar vectorizador y transformar textos en características
+feature_vectors = vectorizer.fit_transform(combined_features)
+
+#Aplicar el algoritmo de cosine similarity a las características
+similarity = cosine_similarity(feature_vectors)
+
+# Definir el nombre de la película usando la caja de texto
+default_option = "Nombre de la película"
+
+#Desplegar caja de selección o inserción de la película
+movie_name = st.selectbox('## SELECCIONE O ESCRIBA EL NOMBRE DE LA PELÍCULA',
+                          [default_option] + movies_data['title'].tolist(), key="movie_name", index=0)
+
+#Almacenar todos los títulos de las películas en nueva variable
+list_of_all_titles = movies_data['title'].tolist()
+
+#Buescar coincidencias entre la película ingreasda y la base de datos
+find_close_match = difflib.get_close_matches(movie_name, list_of_all_titles)
+
+#Cuando la película buscada está en la base de datos
+if len(find_close_match) > 0:
+    
+    #Asignar la coincidencia a una variable
+    close_match = find_close_match[0]
+
+    #Asignación indexada de coincidencia de título de la base de datos y la coincidencia
+    close_match_index = movies_data[movies_data.title == close_match].index[0]
+
+    # Obtener una lista de tuplas que representan los índices y puntajes de similitud
+    # de la película con índice close_match_index en relación con otras películas.
+    similarity_score = list(enumerate(similarity[close_match_index]))
+
+    #Ordenar de manera descendente las similitudes con las pelpiculas
+    sorted_similar_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+
+    #Ajusta el valor del margen según del título de número de películas
+    margin_value = "-50px"
+
+    #Título de caja elección número de películas recomendadas que se desea
+    st.markdown(f'<h3 style="color:white; margin-bottom: {margin_value};">Número de películas</h3>',
+                unsafe_allow_html=True)
+    
+    #Definir mínimo, máximo y valor por defecto del número de películas recomendadas
+    num_recommendations = st.number_input('', min_value=5, max_value=30, value=10, format="%d", key="num_recommendations")
+
+    #Título para mostrar películas recomendadas
+    st.markdown('## Películas recomendadas:')
+
+    #Mostrar los nombres de las películas en 4 columnas
+    cols = st.columns(4)
+
+    #Obtener el índice y el título de mayor a menor nivel de coincidencia
+    for i, movie in enumerate(sorted_similar_movies[:num_recommendations]):
+
+        #Asignar título a variable index
+        index = movie[0]
+
+        #Obtener el título de la película correspondiente al índice index en la base de datos
+        title = movies_data[movies_data.index == index]['title'].values[0]
+ 
+        # Búsqueda aproximada 
+        #Asignar los títulos de result_df a movie_titles
+        movie_titles = result_df['title'].tolist()
+
+        #Closest_match contendrá el título de la película más cercana al título proporcionado en la variable title
+        closest_match = difflib.get_close_matches(title, movie_titles)[0]
+
+        #Asignar a movie_row la fila en result_df de la peelícula más cercana a la ingresada
+        movie_row = result_df[result_df['title'] == closest_match]
+
+        #Asignar el enlace de la película si la fila no está vacía
+        if not movie_row.empty:
+            imdb_link = movie_row['movie_imdb_link'].values[0]
+        
+        #Si la fila está vacía dejar igual
+        else:
+            imdb_link = ""
+
+        #Mostrar título y enlace de cada película recomendada
+        with cols[i % 4]:
+            st.header(title)
+            st.write(f"IMDB: {imdb_link}")
+        
+else:  
+    #Crear estilo con CSS para mostrar advertencia 
+    css = """
+    <style>
+        .warning-box {
+            background-color: #ffcccb; /* Color de fondo personalizado */
+            padding: 1em;
+            border-radius: 10px; /* Bordes redondeados */
+            border: 2px solid #8b0000; /* Color del borde */
+            color: #8b0000; /* Color del texto */
+        }
+    </style>
+    """
+
+    # Inyectar estilos CSS
+    st.markdown(css, unsafe_allow_html=True)
+
+    # Mensaje de advertencia personalizado
+    st.markdown('<div class="warning-box">NO SE ENCONTRÓ UNA COINCIDENCIA APROXIMADA!!</div>', unsafe_allow_html=True)
+
